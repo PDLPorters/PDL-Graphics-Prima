c48d2f25bea5a1c3b47c8bf588f00127cecaa004
David Mertens <dcmertens.perl@gmail.com>
2011-09-20 14:39:53 -0500

    Added ErrorBars plotType toy script

---
+++ test-error-bars.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use PDL;
+use blib;
+use Prima qw(Application);
+use PDL::Graphics::Prima;
+use PDL::NiceSlice;
+
+my $wDisplay = Prima::MainWindow->create(
+	text    => 'Error Bar Test',
+	size	=> [500, 500],
+);
+
+$wDisplay->insert('Plot',
+	-data => [
+		3*grandom(30), 3*grandom(30),
+		plotType => [
+			pt::ErrorBars(y_err => grandom(30), colors => cl::LightRed,
+				x_left_err => grandom(30), x_err => grandom(30)),
+			pt::Blobs(radius => 4),
+		]
+	],
+	pack => { fill => 'both', expand => 1},
+);
+
+run Prima;
+
