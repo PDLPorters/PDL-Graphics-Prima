2db48be5a97bbfb9527c0d3844c8b0abd2792544
David Mertens <dcmertens.perl@gmail.com>
2011-11-11 15:03:37 -0600

    Modified test/simple-plot to use thread_like

--- test/simple-plot.pl
+++ test/simple-plot.pl
@@ -5,10 +5,16 @@ use blib;
 use PDL::Graphics::Prima::Simple -sequential;
 
 my $xs = sequence(100)/10;
-my $ys = sin($xs);
+my $ys = sin($xs) + 3 * sequence(3)->transpose;
+my $colors = pal::Rainbow->apply(sequence(3)->transpose);
 plot(
-	-data => [$xs, $ys],
-	color => cl::LightBlue,
+	-data => [$xs, $ys,
+		plotType => [
+			pt::Blobs,
+			pt::Lines(thread_like => 'points'),
+		],
+		colors => $colors,
+	],
 );
 
 plot(
