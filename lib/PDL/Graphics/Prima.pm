cca43b522bc22ec22d4f87eced02cecd73f29ac9
David Mertens <dcmertens.perl@gmail.com>
2012-03-15 08:08:46 -0500

    Tweaked the abstract

--- lib/PDL/Graphics/Prima.pm
+++ lib/PDL/Graphics/Prima.pm
@@ -23,7 +23,7 @@ use PDL::Graphics::Prima::DataSet;
 
 =head1 NAME
 
-PDL::Graphics::Prima - an interactive graph widget for PDL and Prima
+PDL::Graphics::Prima - an interactive plotting widget and library for PDL and Prima
 
 =head1 SIMPLE SYNOPSIS
 
