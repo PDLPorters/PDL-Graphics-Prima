c1ca160a8a9651cb0705f7e8b04b7da964d8579d
David Mertens <dcmertens.perl@gmail.com>
2013-03-10 00:13:02 -0600

    Put some real work into Palette, finally

--- lib/PDL/Graphics/Prima/Palette.pm
+++ lib/PDL/Graphics/Prima/Palette.pm
@@ -13,6 +13,50 @@ you want the color to represent the topography of a landscape, darker is
 lower, lighter is higher. In that case, you need a mapping from a height to
 a color, i.e. from a scalar value to a color. This is what palettes provide.
 
+If all you need is basic palette, you can use one of the
+L<palette builders provided below|/"Special Palettes">. That said, creating
+custom color palettes, when you have some idea of what you're doing and a
+simple means for doing so, is a lot of fun. This, for example, creates a
+palette that runs from black to red. You could just use
+L<pal::BlackToHSV|/pal::BlackToHSV>, but what's the fun in that?
+
+ my $palette = PDL::Graphics::Prima::Palette->new(
+     apply => sub {
+         my $data = shift;
+         my ($min, $max) = $data->minmax;
+         
+         # Build the rgb piddle
+         my $rgb = zeroes(3, $data->dims);
+         $rgb->slice("0") .= (($data->double - $min) / ($max - $min)) * 255;
+         
+         # Convert to Prima colors
+         return $rgb->rgb_to_color;
+     }
+ );
+
+Applying the palette to some data simply calls the subref that your provided
+earlier:
+
+ my $colors = $palette->apply($some_data);
+
+Using this with a standard palette builder is pretty easy, too:
+
+ my $colors = pal::Rainbow->apply($some_data);
+
+And, you can provide the palette to customize how
+L<pgrid::Matrix|PDL::Graphics::Prima::PlotType/pgrid::Matrix>
+colorizes its data:
+
+ plot(
+     -data => ds::Grid( $matrix,
+         plotType => pgrid::Matrix(palette => $palette),
+         bounds => [0, 0, 1, 1],
+     )
+ );
+
+=for future
+All Palette classes know how to make new copies of themselves... ?
+
 =cut
 
 # At this point, I am debating just how closely I want Palettes to interact
@@ -32,9 +76,9 @@ use Carp;
 
 =head2 new
 
-You will almost always create palettes using the nick-names in the C<pal>
-namespace. However, all Palette classes know how to make new copies of
-themselves.
+Accepts key/value pairs. The only required key is the C<apply> key, which
+should have a coderef that accepts a data piddle and performs the
+data-to-color conversion, returning a piddle of Prima colors.
 
 =cut
 
@@ -71,9 +115,9 @@ Every Palette knows the specific data and plot type to which it belongs.
 The first time that a Palette is used in a drawing operation, it will become
 associated with that specific plotType object, which is in turn associated
 with that specific dataSet and widget. Thereafter, you can retrieve the
-plotType object using this accessor, but you cannot change it. If you want
-to use the same Palette on a different plotType, you can copy it using
-C<pal::Copy>.
+plotType object using this accessor, but you cannot change it.
+If you want to use the same Palette with a different plotType, you can
+create a copy of your palette using the L</copy> method.
 
 =cut
 
@@ -102,24 +146,26 @@ sub plotType {
 
 =head2 copy
 
-Not yet implemented.
+You can make a copy of a Palette that is identical to your current pallete
+except that it does not have an associated plotType. This way, if you put a
+lot of effort into making a palette, you can easily reuse that palette with
+minimal effort.
 
-You can make a copy of a Palette that does not have an associated plotType.
-This way, if you put a lot of effort into making a palette, you can easily
-reuse that palette with minimal effort.
+Note that this mechanism does not perform a deep copy, and any nested data
+structures will be copied by reference to the new palette object.
 
 =cut
 
 sub copy {
-	die "working here";
+	my $self = shift;
+	my $new_guy = { %$self };
+	delete $new_guy->{plotType};
+	return $new_guy;
 }
 
 package PDL::Graphics::Prima::Palette::HSVrange;
 our @ISA = qw(PDL::Graphics::Prima::Palette);
 use Carp;
-
-# working here - consider a constructor that knows how to deal with 
-# piddles that have a width of 3?
 
 sub new {
 	my $class = shift;
@@ -134,8 +180,11 @@ sub new {
 		s_stop  => 1,
 		v_start => 1,
 		v_stop  => 1,
+		gamma   => 1,
 		@_
 	};
+	croak("${class}::new expects positive gamma values")
+		if $self->{gamma} <= 0;
 	return bless $self, $class;
 }
 
@@ -145,7 +194,9 @@ sub apply {
 	# Get the data and scale it from zero to one, taking care to correctly
 	# handle collections of identical values:
 	my ($min, $max) = $data->minmax;
-	my $scaled_data = $min == $max ? $data->zeroes : ($data - $min) / ($max - $min);
+	my $scaled_data = $min == $max ? $data->zeroes
+						: (($data->double - $min) / ($max - $min));
+	$scaled_data **= $self->{gamma};
 	
 	# Compute the associated hue, saturation, and vaue:
 	my $h = $scaled_data * ($self->{h_stop} - $self->{h_start})
@@ -179,11 +230,14 @@ sub pal::Rainbow {
 
 =item pal::BlackToWhite
 
-Larger values are white, smaller values are black
+Larger values are white, smaller values are black. The optional argument is
+the gamma exponent correction value, which should be positive. Typically,
+gamma exponents are near 0.5.
 
 =cut
 
 sub pal::BlackToWhite {
+	my $gamma = shift || 1/2.2;
 	return PDL::Graphics::Prima::Palette::HSVrange->new(
 		h_start => 0,
 		h_stop  => 0,
@@ -191,16 +245,20 @@ sub pal::BlackToWhite {
 		s_stop  => 0,
 		v_start => 0,
 		v_stop  => 1,
+		gamma   => $gamma,
 	);
 }
 
 =item pal::WhiteToBlack
 
-Larger values are black, smaller values are white
+Larger values are black, smaller values are white. The optional argument is
+the gamma exponent correction value, which should be positive. Typically,
+gamma exponents are near 0.5.
 
 =cut
 
 sub pal::WhiteToBlack {
+	my $gamma = shift || 1/2.2;
 	return PDL::Graphics::Prima::Palette::HSVrange->new(
 		h_start => 0,
 		h_stop  => 0,
@@ -208,6 +266,7 @@ sub pal::WhiteToBlack {
 		s_stop  => 0,
 		v_start => 1,
 		v_stop  => 0,
+		gamma   => $gamma,
 	);
 }
 
@@ -215,16 +274,20 @@ sub pal::WhiteToBlack {
 
 Smaller values are closer to white, larger values are closer to the color
 indicated by the HSV values that you specify, which are supplied to the
-function as three different scalars.
+function as three different scalars. The first three arguments are hue,
+saturation, and value. The optional fourth value is a gamma correction
+exponent.
 
 For example:
 
- my ($h, $s, $v) = pdl(cl::LightRed)->color_to_rgb->rgb_to_hsv->dog;
- my $white_to_red = pal::WhiteToHSV($h, $s, $v);
+ my $white_to_red = pal::WhiteToHSV(0, 1, 1);
+ my $gamma_white_to_red = pal::WhiteToHSV(0, 1, 1, 0.8);
 
 =cut
 
 sub pal::WhiteToHSV {
+	my @gamma;
+	@gamma = (gamma => pop) if @_ == 4;
 	return PDL::Graphics::Prima::Palette::HSVrange->new(
 		h_start => 0,
 		s_start => 0,
@@ -232,6 +295,7 @@ sub pal::WhiteToHSV {
 		h_stop  => $_[0],
 		s_stop  => $_[1],
 		v_stop  => $_[2],
+		@gamma,
 	);
 }
 
@@ -242,6 +306,8 @@ Like WhiteToHSV, but smaller values are closer to black instead of white.
 =cut
 
 sub pal::BlackToHSV {
+	my @gamma;
+	@gamma = (gamma => pop) if @_ == 4;
 	return PDL::Graphics::Prima::Palette::HSVrange->new(
 		h_start => 0,
 		s_start => 0,
@@ -249,32 +315,100 @@ sub pal::BlackToHSV {
 		h_stop  => $_[0],
 		s_stop  => $_[1],
 		v_stop  => $_[2],
+		@gamma,
 	);
 }
 
 =item pal::HSVrange
 
-Goes in ascending order from the start to the stop values in hue, saturation,
-and value. All values are specified with key => value pairs. For example, to
-go from blue to red:
+Maps data in ascending order from the start to the stop values in hue, saturation,
+and value. You can specify the initial and final hue, saturation, and value
+in one of two ways: (1) a pair of three-element arrayrefs/piddles with the
+initial and final hsv values, or (3) a set of key/value pairs describing the initial
+and final hue, saturation and value.
 
- my ($start_h, $start_s, $start_v)
-   = pdl(cl::LightBlue)->color_to_rgb->rgb_to_hsv->dog;
- my ($stop_h, $stop_s, $stop_v)
-   = pdl(cl::LightRed)->color_to_rgb->rgb_to_hsv->dog;
- 
+For example, this creates a palette that runs from red (H=360) to blue
+(H=240):
+
+ my $blue_to_red = pal::HSVrange([1, 1, 360] => [1, 1, 240]);
+
+If you know the L<Prima name of your color|Prima::Const/cl>, you can use the
+conversion functions provided by
+L<PDL::Drawing::Prima::Utils|PDL::Drawing::Prima::Utils/> to build an HSV
+range. This example produces a palette from blue to red:
+
+ my $blue_hsv = pdl(cl::LightBlue)->color_to_rgb->rgb_to_hsv;
+ my $red_hsv = pdl(cl::LightRed)->color_to_rgb->rgb_to_hsv;
+ my $blue_to_red = pal::HSVrange($blue_hsv, $red_hsv);
+
+The final means for specifying a range in HSV space is to provide key/value
+pairs that describe your initial and final points in HSV space. You can
+also specify a non-unitary gamma correction exponent. For example,
+to go from blue to red with a gamma of 0.8, you could say:
+
  my $blue_to_red = pal::HSVrange(
-       h_start => $start_h,
-       s_start => $start_s,
-       v_start => $start_v,
-       h_stop  => $stop_h,
-       s_stop  => $stop_s,
-       v_stop  => $stop_v,
+       h_start => 240,
+       s_start => 1,
+       v_start => 1,
+       h_stop  => 360,
+       s_stop  => 1,
+       v_stop  => 1,
+       gamma   => 0.8,
    );
+
+However, you do not need to provide all of these values. Any key that you do
+not supply will use a default value:
+
+ Key       Default
+ -----------------
+ h_start   0
+ s_start   1
+ v_start   1
+ h_stop    360
+ s_stop    1
+ v_stop    1
+ gamma     1
+
+So the blue-to-red palette, without a gamma correction, could be specified
+as:
+
+ my $blue_to_red = pal::HSVrange(
+     h_start => 240, h_stop => 360,
+ );
 
 =cut
 
+use Scalar::Util qw(blessed);
 sub pal::HSVrange {
+	croak("pal::HSVrange called with odd number of arguments")
+		if @_ % 2 == 1;
+	
+	# replace piddles in either of the fist two args with arrayrefs
+	$_[0] = [$_[0]->dog] if blessed($_[0]) and $_[0]->isa('PDL');
+	$_[1] = [$_[1]->dog] if blessed($_[1]) and $_[1]->isa('PDL');
+	
+	# Did they provide two array refs?
+	if (ref($_[0]) and ref($_[0]) eq ref([])) {
+		my $from = shift;
+		my $to = shift;
+		
+		# Check that $to is also an array ref, and that the dims are correct
+		ref($to) and ref($to) eq ref([])
+			or croak('If the first argument to pal::HSVrange is an array ref'
+				. ' or piddle, the second should be an array ref or piddle, too');
+		@$from == 3 and @$to == 3
+			or croak('If called with to/from array refs or piddles, pal::HSVrange'
+				. ' expects three-elements in said array refs or piddles');
+		# Pad the argument list in preparation for the call to new
+		unshift @_,
+			h_start => $from->[0],
+			s_start => $from->[1],
+			v_start => $from->[2],
+			h_stop  => $to->[0],
+			s_stop  => $to->[1],
+			v_stop  => $to->[2],
+	}
+	
 	return PDL::Graphics::Prima::Palette::HSVrange->new(@_);
 }
 
@@ -354,3 +488,20 @@ This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
 =cut
+
+__END__
+
+# Basic histogram equalization implementation, worth considering:
+# See http://www.generation5.org/content/2004/histogramEqualization.asp
+
+# Let's try histogram equalization on this
+my $xs = $m51->flat->qsort;
+# Find the bottom 10% and top 10% and clip by them
+$m51->flat .= $m51->flat->vsearch($xs);
+my $ys = $xs->xlinvals(0, 1);
+line_plot($xs, $ys);
+
+# Interesting clipping idea:
+my $min = $xs->at($xs->nelem/10);
+my $max = $xs->at($xs->nelem*0.9);
+$m51->clip($min, $max);
