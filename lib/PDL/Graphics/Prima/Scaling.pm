d2243d0abe1a6d3b8f1f8c634254eafe6c26ab94
David Mertens <dcmertens.perl@gmail.com>
2014-08-25 16:54:58 -0400

    Updated the copyright information

--- lib/PDL/Graphics/Prima/Scaling.pm
+++ lib/PDL/Graphics/Prima/Scaling.pm
@@ -509,14 +509,16 @@ plots
 Unless otherwise stated, all contributions in code and documentation are
 copyright (c) their respective authors, all rights reserved.
 
-Portions of this module's code are copyright (c) 2011 The Board of Trustees at
-the University of Illinois.
+Portions of this module's code are copyright (c) 2011 The Board of
+Trustees at the University of Illinois.
 
 Portions of this module's code are copyright (c) 2011-2013 Northwestern
 University.
 
-Portions of this module's documentation are copyright (c) 2011-2013 David
-Mertens.
+Portions of this module's code are copyright (c) 2013-2014 Dickinson
+College.
+
+This module's documentation is copyright (c) 2011-2014 David Mertens.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
